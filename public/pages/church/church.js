5a807c68ce5310965f4b7c18d9687183e5672435
Ashot <axabekyan.ashot.2003@gmail.com>
2024-12-06 02:40:36 +0400

    church page 'faith basics' component animation feat added, minor changes

--- public/pages/church/church.js
+++ public/pages/church/church.js
@@ -3,14 +3,12 @@ const textWrapper = document.getElementById("bibleTextContainer");
 const toggleBtn = document.getElementById("more-less-btn")
 const hiddenElements = document.querySelectorAll(".hiddenText");
 
-
 toggleBtn.addEventListener("click", function() {
     if (textWrapper.classList.contains("collapsed")) {
         textWrapper.classList.remove("collapsed");
         
         hiddenElements.forEach(function(elem) {
             elem.classList.toggle('visible');
-            elem.classList.remove("hiddenText");
         });
         toggleBtn.innerText = "Փակել";
     }
@@ -18,7 +16,6 @@ toggleBtn.addEventListener("click", function() {
         textWrapper.classList.add("collapsed");
         hiddenElements.forEach(function(elem) {
             elem.classList.toggle('visible');
-            elem.classList.add("hiddenText");
         });
         window.scroll({"top": "500", "behavior": "smooth"});
         toggleBtn.innerText = "Ավելին";
