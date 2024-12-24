220e00e6bcab7424bd6f5d848ee83e4e6d1c5778
Ashot <axabekyan.ashot.2003@gmail.com>
2024-12-24 13:59:03 +0400

    home pahe full refactoring by BEM

--- public/pages/home/home.js
+++ public/pages/home/home.js
@@ -1,7 +1,7 @@
 "use strict";
 
-let toDonationPageBtn = document.getElementById("donationsBtn");
+let toDonationPageBtn = document.querySelector(".home__donations-btn");
 toDonationPageBtn.addEventListener("click", () => window.location.href = "donation");
 
-let toVideosPageBtn = document.getElementById("videosBtn");
+let toVideosPageBtn = document.querySelector(".home__videos-btn");
 toVideosPageBtn.addEventListener("click", () => window.location.href = "videos/4");
